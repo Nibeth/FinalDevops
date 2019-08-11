6ce3d04d10c324e52a2c21317d6642602076a0e3
Nibeth Mena <nmena@corp.dharbor.com>
2019-08-11 19:53:25 -0400

    unit test3

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,9 +57,9 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-import 'core-js/es6/object';
+/*import 'core-js/es6/object';
 import 'core-js/es6/array';
-import 'intl';  // Run `npm install --save intl`.
+import 'intl';  // Run `npm install --save intl`.*/
 
 /*
 import 'core-js/es6/object';
